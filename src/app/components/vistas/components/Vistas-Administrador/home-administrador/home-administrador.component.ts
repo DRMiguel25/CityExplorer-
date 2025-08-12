0e532e23d945ef08f39f9415d2fc1e8bbab3b1a2
José Manuel García Morales <conoplant2@gmail.com>
2025-08-12 14:04:19 -0600

    solucion de bugs

--- src/app/components/vistas/components/Vistas-Administrador/home-administrador/home-administrador.component.ts
+++ src/app/components/vistas/components/Vistas-Administrador/home-administrador/home-administrador.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpLaravelService } from "../../../../../http.service";
 import { ActivatedRoute } from '@angular/router';
+import { Lugar } from './lugar.interface';
+import { MatDialog } from '@angular/material/dialog';
+import { AlertaInfoUsuarioComponent } from '../../Vistas-Usuario-Invitado/alerta-info-usuario/alerta-info-usuario.component';
 
 @Component({
   selector: 'home-administrador',
@@ -12,15 +15,35 @@ import { ActivatedRoute } from '@angular/router';
 export class HomeAdministradorComponent implements OnInit{
   idUsuario: number = 0;
 
+  usuario: any = null; // Aquí vamos a guardar la info para mostrarla en el HTML
+
+  nombreCompleto: any = null;
+
+  usuarios: any[] = [];
+  usuariosfiltrados: any[] = [];
+
+  totalUsuarios: number = 0;
+  usuariosActivos: number = 0; // 👈 aquí guardamos la cuenta de los activos
+  usuariosBloqueados: number = 0; // 👈 aquí guardamos la cuenta de los activos
+
+  lugares: Lugar[] = [];
+
+  totalLugares: number = 0;
+
   constructor(
     private service: HttpLaravelService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private dialog: MatDialog,
   ){}
   ngOnInit(): void {
     this.logLoadTime();  // 👈 mide tiempo de carga
     this.idUsuario = Number(this.route.snapshot.paramMap.get('id_usuario'));
     console.log("id del usuario: "+this.idUsuario);
+
+    this.cargarInfoUsuario();
+    this.cargarUsuarios();
+    this.loadLugares();
   }
 
   logLoadTime() {
@@ -50,5 +73,63 @@ export class HomeAdministradorComponent implements OnInit{
   Ayuda() {
     this.router.navigate(['/ayuda-administrador', this.idUsuario]);
   }
+
+  PanelDeControl(){
+    this.dialog.open(AlertaInfoUsuarioComponent, {
+      width: '450px',
+      data: { id_usuario: this.idUsuario },
+      autoFocus: true // opcional: enfoca al abrir
+    });
+  }
+
+  cargarInfoUsuario() {
+    this.service.Service_Get('usuario', this.idUsuario).subscribe(
+      (respuesta: any) => {  // 👈 Cast a any aquí, no tocamos el servicio
+        if (respuesta.estatus === 1) {
+          this.usuario = respuesta.data;
+          console.log('✅ Usuario:', this.usuario);
+          this.nombreCompleto = (this.usuario.nombre+" "+this.usuario.apellidoP+" "+this.usuario.apellidoM)
+        } else {
+          console.warn('⚠️ La API respondió sin éxito:', respuesta);
+        }
+      },
+      error => {
+        console.error('❌ Error al obtener usuario:', error);
+      }
+    );
+  }
+
+    cargarUsuarios(): void {
+    this.service.Service_Get_Usuarios_Admin().subscribe({
+      next: (respuesta) => {
+        console.log('📦 Usuarios recibidos:', respuesta);
+        this.usuarios = respuesta.data; // 👈 solo la lista de usuarios
+
+        this.usuariosfiltrados = respuesta.data; // 👈 solo la lista de usuarios filtrados
+
+        this.totalUsuarios = respuesta.total; // 👈 total del paginador
+        this.usuariosActivos = this.usuarios.filter(u => u.activo === true).length;
+        this.usuariosBloqueados = this.usuarios.filter(u => u.bloqueado === true).length;
+
+      },
+      error: (error) => {
+        console.error('❌ Error al cargar usuarios:', error);
+      }
+    });
+  }
+
+loadLugares(): void {
+  this.service.Service_Get('lugar', '').subscribe({
+    next: (data: Lugar[]) => {
+      this.lugares = data;
+      this.totalLugares = data.length; // 📌 Aquí tienes el total
+      console.log("Total de lugares:", this.totalLugares);
+      console.log("Data de lugares:", this.lugares);
+    },
+    error: (error) => {
+      console.error('❌ Error al cargar lugares:', error);
+    }
+  });
+}
 
 }
